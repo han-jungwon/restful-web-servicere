84f4153ddb169b8263fc89f3a217390bc478a2c9
han-jungwon <rhehf1024@naver.com>
2022-10-06 00:28:41 +0900

    Response데이터 제어를 위한 Filtering

--- src/main/java/com/example/restfulwebservice/user/UserDaoService.java
+++ src/main/java/com/example/restfulwebservice/user/UserDaoService.java
@@ -14,9 +14,9 @@ public class UserDaoService {
     private static int usersCount = 3;
 
     static {
-        users.add(new User(1, "Kenneth", new Date()));
-        users.add(new User(2, "JungWon", new Date()));
-        users.add(new User(3, "Cola", new Date()));
+        users.add(new User(1, "Kenneth", new Date(), "pass1", "801010-1111111"));
+        users.add(new User(2, "JungWon", new Date(), "pass2", "701010-1111111"));
+        users.add(new User(3, "Cola", new Date(), "pass3", "901010-1111111"));
     }
 
     public List<User> findAll() {
